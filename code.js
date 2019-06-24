60dffed775b93a93f2b0d75446c9c4a18dfaa1a1
basiclines <igonzaleznicolas@gmail.com>
2019-06-24 19:06:21 +0200

    Improved items selection UI

--- code.js
+++ code.js
@@ -1,38 +1,30 @@
-function getTextNodesFrom(selection) {
-	var nodes = []
+var MIN_HEIGHT = 480
+var MAX_HEIGHT = 640
 
-	function childrenIterator(node) {
-		if (node.children) {
-			node.children.forEach(child => {
-				childrenIterator(child)
-			})
-		} else {
-			if (node.type === 'TEXT') nodes.push({ id: node.id, characters: node.characters })
-		}
-	}
-
-	selection.forEach(item => childrenIterator(item))
-	return nodes
-}
-
-
-figma.showUI(__html__, { width: 320, height: 248 });
+figma.showUI(__html__, { width: 320, height: MAX_HEIGHT });
 
 figma.ui.onmessage = msg => {
 
 	if (msg.type === 'pluginReady') {
 		var selection = figma.currentPage.selection
-		var textNodes = getTextNodesFrom(selection)
-		// var textNodes = figma.currentPage.findAll(n => n.type === 'TEXT')
+		var textNodes = figma.currentPage.findAll(n => n.type === 'TEXT')
 		var uniques = {}
 
 		textNodes.map(item => {
 			if (typeof uniques[item.characters] != 'undefined') {
-				uniques[item.characters].push(item)
+				uniques[item.characters].push(item.id)
 			} else {
-				uniques[item.characters] = [item]
+				uniques[item.characters] = [item.id]
 			}
 		})
+
+		console.log(uniques)
+
+		var orderedUniques = []
+		for (var item in uniques) {
+			console.log(item, uniques[item])
+		}
+
 
 		var message = { type: 'render', uniques: uniques }
 		figma.ui.postMessage(message)
