b88803200d18e0a2f597281118e0d8c45927d8f1
basiclines <igonzaleznicolas@gmail.com>
2019-06-24 20:22:21 +0200

    Added selection manipulation

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 var MIN_HEIGHT = 480
 var MAX_HEIGHT = 640
+var orderedUniques = []
+var initialSelection = figma.currentPage.selection.slice(0)
 
 figma.showUI(__html__, { width: 320, height: MAX_HEIGHT });
 
@@ -18,10 +20,10 @@ figma.ui.onmessage = msg => {
 			}
 		})
 
-		var orderedUniques = []
 		for (var item in uniques) {
 			orderedUniques.push({ key: item, nodes: uniques[item] })
 		}
+
 		orderedUniques.sort(function(a, b) {
 			var nameA = a.key.toUpperCase()
 			var nameB = b.key.toUpperCase()
@@ -37,6 +39,23 @@ figma.ui.onmessage = msg => {
 		var message = { type: 'render', uniques: orderedUniques }
 		figma.ui.postMessage(message)
 	}
+
+	if (msg.type === 'previewNodes') {
+		var idx = msg.options
+		var nodes = orderedUniques[idx].nodes
+		var previewNodes = nodes.reduce((buffer, item) => {
+			 buffer.push(figma.getNodeById(item))
+			 return buffer
+		}, [])
+		figma.viewport.scrollAndZoomIntoView(previewNodes)
+		figma.currentPage.selection = previewNodes
+	}
+
+	if (msg.type === 'restoreSelection') {
+		figma.viewport.scrollAndZoomIntoView(initialSelection)
+		figma.currentPage.selection = initialSelection
+	}
+
 
 	  // figma.closePlugin();
 }
